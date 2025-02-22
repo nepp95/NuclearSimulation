c376b12a6c1520a3fd357b77039cfb872cc86d8d
Niels Eppenhof <n.eppenhof@gmail.com>
2025-02-22 14:42:09 +0100

    upd

--- Source/NuclearSimulationLayer.cpp
+++ Source/NuclearSimulationLayer.cpp
@@ -1,18 +1,65 @@
 #include "NuclearSimulationLayer.h"
 
+#include <EppoCore/Core/Timer.h>
+#include <EppoCore/Platform/GUI/ApplicationGUI.h>
+
 #include <imgui.h>
 
-NuclearSimulationLayer::NuclearSimulationLayer()
-{}
-
 void NuclearSimulationLayer::OnAttach()
-{}
+{
+    // Disable V-Sync
+    Window::SetVSync(false);
+
+    // Create framebuffer we will render to
+    FramebufferSpecification spec{
+        .Width = 1280,
+        .Height = 720,
+    };
+
+    m_Framebuffer = std::make_unique<Framebuffer>(spec);
+}
 
 void NuclearSimulationLayer::OnDetach()
 {}
 
 void NuclearSimulationLayer::OnUpdate(float timestep)
-{}
+{
+    // Logic
+    Timer timer;
+
+    m_LastUpdateTime = timer.GetElapsedMilliseconds();
+    timer.Reset();
+
+    // Rendering
+    const auto renderer = Application::Get().GetRenderer();
+
+    m_Framebuffer->Bind();
+
+    renderer->BeginScene(glm::ortho(-1.0f, 1.0f, -1.0f, 1.0f, -1.0f, 1.0f));
+
+    // Data visualisation start
+    renderer->DrawRectangle(glm::vec2(-0.75f, -0.5f), glm::vec2(0.5f, 1.0f), glm::vec4(1.0f, 0.0f, 0.0f, 1.0f));
+
+    // Visualisation start
+    renderer->DrawRectangle(glm::vec2(0.0f, 0.5f), glm::vec2(2.0f, 1.0f), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
+
+    // Graphite moderators
+    for (uint32_t i = 0; i < 10; i++)
+        renderer->DrawRectangle(glm::vec2(-0.90f + 0.2f * static_cast<float>(i), 0.75f), glm::vec2(0.1f, 0.5f),
+                                glm::vec4(0.2f, 0.2f, 0.2f, 1.0f));
+
+    // Control rods
+    for (uint32_t i = 0; i < 9; i++)
+        renderer->DrawRectangle(glm::vec2(-0.8f + 0.2f * static_cast<float>(i), 0.25f), glm::vec2(0.05f, 0.5f),
+                                glm::vec4(0.0f, 0.0f, 0.0f, 1.0f));
+    //
+
+    renderer->EndScene();
+
+    Framebuffer::Unbind();
+
+    m_LastFrameTime = timer.GetElapsedMilliseconds();
+}
 
 void NuclearSimulationLayer::OnUIRender()
 {
@@ -52,10 +99,18 @@ void NuclearSimulationLayer::OnUIRender()
     }
 
     ImGui::Begin("Settings");
+    ImGui::Text("Update time: %.3fms", m_LastUpdateTime);
+    ImGui::Text("Render time: %.3fms", m_LastFrameTime);
     ImGui::End();
 
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
     ImGui::Begin("Reactor");
+
+    const auto size = ImGui::GetContentRegionAvail();
+    ImGui::Image((ImTextureID)m_Framebuffer->GetRendererID(), size);
+
     ImGui::End();
+    ImGui::PopStyleVar();
 
     //
     // End of docking space
--- Source/NuclearSimulationLayer.h
+++ Source/NuclearSimulationLayer.h
@@ -1,13 +1,18 @@
 #pragma once
 
+#include "Reactor.h"
+
 #include <EppoCore/Core/Application.h>
+#include <EppoCore/Platform/GUI/Framebuffer.h>
+#include <EppoCore/Platform/GUI/Shader.h>
+#include <EppoCore/Platform/GUI/Vertex.h>
 
 using namespace Eppo;
 
-class NuclearSimulationLayer : public Layer
+class NuclearSimulationLayer final : public Layer
 {
 public:
-    NuclearSimulationLayer();
+    NuclearSimulationLayer() = default;
     ~NuclearSimulationLayer() override = default;
 
     void OnAttach() override;
@@ -15,4 +20,15 @@ public:
 
     void OnUpdate(float timestep) override;
     void OnUIRender() override;
+
+private:
+    float m_LastFrameTime = 0.0f;
+    float m_LastUpdateTime = 0.0f;
+
+    std::unique_ptr<Framebuffer> m_Framebuffer;
+    std::unique_ptr<Shader> m_Shader;
+
+    std::vector<Vertex> m_Vertices;
+
+    Reactor m_Reactor;
 };
