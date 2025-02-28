a357d888350f6d6be2598386f2a96e29d5cca8e9
Niels Eppenhof <n.eppenhof@gmail.com>
2025-02-28 01:05:06 +0100

    Simple, but it works

--- Source/NuclearSimulationLayer.cpp
+++ Source/NuclearSimulationLayer.cpp
@@ -27,63 +27,31 @@ void NuclearSimulationLayer::OnUpdate(const float timestep)
     // Logic
     Timer timer;
 
-    // Timestep in seconds
-    float dt = timestep / 1000.0f;
-    t += dt;
-
-    dt = 0.01f;
-
-    // Constants
-    constexpr float Q0 = 0.05f * 3200.0f;
-    constexpr float B = 0.0065f; // Delayed neutron fraction
-    constexpr float l = 0.08f; // Decay constant
-    constexpr float L = 0.001f; // Neutron generation time
-    constexpr float MeV = 200.0f; // Energy per fission
-    constexpr float MeV_W = 1.60218e-13f; // Conversion factor (MeV to watts)
-    constexpr float tau = 100.0f; // Decay heat time constant
-    constexpr float C_heat = 1e6f; // Heat capacity of the core
-    constexpr float R_thermal = 1e4f; // Thermal resistance
-    constexpr float C_coolant = 4.18e3f; // Coolant heat capacity
-    constexpr float M_coolant = 200.0f; // Coolant mass
-    constexpr float T_alpha = -2e-4f; // Temperature coefficient of reactivity per C
-    constexpr float k_rod = 0.02f; // Control rod efficiency
-    constexpr float f_rate = 1.0f; // Fission rate
-
     // Reactivity
-    float d = m_ControlRodInsertion / 100.0f; // value is between 0 and 100
-    float p_rod = -k_rod * d;
-    p = p0 + T_alpha * (T - T0) + p_rod;
+    const float prod = m_ControlRodInsertion / 100.0f * (0.2f - -0.2f) + -0.2f;
+    p = prod * a * (T - T0);
 
-    // Neutron population
-    int64_t dN = (p - B) / L * N + l * C;
-    N = N + dN * dt;
+    // Neutron Density
+    const float dN = (p / A) * N * dt;
+    N += dN;
 
-    // Precursor concentration
-    int64_t dC = B / L * N - l * C;
-    C = C + dC * dt;
+    // Core Temperature
+    const float dT = (e * N - y * (T - Tenv)) * dt;
+    T += dT;
 
     // Power
-    P = N * MeV * MeV_W * f_rate;
-
-    // Decay heat
-    Q = Q0 * exp(-t / tau);
-
-    // Temperature
-    float dT = (P + Q) / C_heat - (T - T_coolant) / R_thermal;
-    T = T + dT * dt;
-
-    // Coolant temperature
-    float heatTransfer = P + Q;
-    float dT_coolant = heatTransfer / (C_coolant * M_coolant);
-    T_coolant = T_coolant + dT_coolant * dt;
+    P = N;
 
     // Auto balance
-    if (P < 2000.0f)
-        m_ControlRodInsertion -= m_ControlRodSpeed * dt;
-    else
-        m_ControlRodInsertion += m_ControlRodSpeed * dt;
+    if (m_AutomaticControl)
+    {
+        if (P < Ptarget && T < 550.0f && p < 0.00002f)
+            m_ControlRodInsertion -= m_ControlRodSpeed * timestep;
+        else if (p > -0.00003f)
+            m_ControlRodInsertion += m_ControlRodSpeed * timestep;
 
-    m_ControlRodInsertion = std::clamp(m_ControlRodInsertion, 0.0f, 100.0f);
+        m_ControlRodInsertion = std::clamp(m_ControlRodInsertion, 0.0f, 100.0f);
+    }
 
     // Rendering
     m_LastUpdateTime = timer.GetElapsedMilliseconds();
@@ -101,10 +69,7 @@ void NuclearSimulationLayer::OnUpdate(const float timestep)
 
     renderer->DrawString(glm::vec2(20.0f, 130.0f), std::format("Power: {:.2f} MW", P));
     renderer->DrawString(glm::vec2(20.0f, 170.0f), std::format("Temp: {:.2f} C", T));
-    renderer->DrawString(glm::vec2(480.0f, 170.0f), std::format("Coolant Temp: {:.2f} C", T_coolant));
     renderer->DrawString(glm::vec2(20.0f, 210.0f), std::format("Reactivity: {:.5f}", p));
-    renderer->DrawString(glm::vec2(20.0f, 250.0f), std::format("Neutron pop: {}", N));
-    renderer->DrawString(glm::vec2(20.0f, 290.0f), std::format("Precursor Conc: {}", C));
 
     // Visualisation start
     renderer->DrawRectangle(glm::vec2(640.0f, 540.0f), glm::vec2(1280.0f, 360.0f), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
@@ -112,25 +77,25 @@ void NuclearSimulationLayer::OnUpdate(const float timestep)
     // Graphite moderators
     for (uint32_t i = 0; i < 10; i++)
     {
-        const float x = (-0.90f + 0.2f * static_cast<float>(i) + 1.0f) / 2.0f * 1280;
-        constexpr float y = (0.75f + 1.0f) / 2.0f * 720;
+        const float xPos = (-0.90f + 0.2f * static_cast<float>(i) + 1.0f) / 2.0f * 1280;
+        constexpr float yPos = (0.75f + 1.0f) / 2.0f * 720;
         constexpr float width = 0.05f * 1280;
         constexpr float height = 0.25f * 720;
 
-        renderer->DrawRectangle(glm::vec2(x, y), glm::vec2(width, height), glm::vec4(0.2f, 0.2f, 0.2f, 1.0f));
+        renderer->DrawRectangle(glm::vec2(xPos, yPos), glm::vec2(width, height), glm::vec4(0.2f, 0.2f, 0.2f, 1.0f));
     }
 
     // Control rods
     for (uint32_t i = 0; i < 9; i++)
     {
         const float controlRodHeight = 0.25f + m_ControlRodInsertion / 100.0f * 0.5f;
-        const float x = (-0.8f + 0.2f * static_cast<float>(i) + 1.0f) * 0.5f * 1280;
-        const float y = (controlRodHeight + 1.0f) * 0.5f
+        const float xPos = (-0.8f + 0.2f * static_cast<float>(i) + 1.0f) * 0.5f * 1280;
+        const float yPos = (controlRodHeight + 1.0f) * 0.5f
             * 720;
         constexpr float width = 0.025f * 1280; // Correct width for Control rods
         constexpr float height = 0.25f * 720;
 
-        renderer->DrawRectangle(glm::vec2(x, y), glm::vec2(width, height), glm::vec4(0.0f, 0.0f, 0.0f, 1.0f));
+        renderer->DrawRectangle(glm::vec2(xPos, yPos), glm::vec2(width, height), glm::vec4(0.0f, 0.0f, 0.0f, 1.0f));
     }
 
     renderer->EndScene();
@@ -178,15 +143,22 @@ void NuclearSimulationLayer::OnUIRender()
     }
 
     ImGui::Begin("Settings");
-    ImGui::Text("Update time: %.5fms", m_LastUpdateTime);
-    ImGui::Text("Render time: %.5fms", m_LastFrameTime);
+
+    ImGui::Text("Control Rod Level: %.2f", m_ControlRodInsertion);
+    ImGui::Checkbox("Automatic Control", &m_AutomaticControl);
+
+    constexpr ImGuiSliderFlags flags = ImGuiSliderFlags_AlwaysClamp;
+    ImGui::SliderFloat("##ControlRod", &m_ControlRodInsertion, 0.0f, 100.0f, "%.2f", flags);
+    ImGui::Text("Target Power Level");
+    ImGui::SliderFloat("##PowerLevel", &Ptarget, 0.0f, 3200.0f, "%.0f", flags);
+
     ImGui::End();
 
     ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
     ImGui::Begin("Reactor");
 
     const auto size = ImGui::GetContentRegionAvail();
-    ImGui::Image((ImTextureID)m_Framebuffer->GetRendererID(), size);
+    ImGui::Image(reinterpret_cast<ImTextureID>(m_Framebuffer->GetRendererID()), size);
 
     ImGui::End();
     ImGui::PopStyleVar();
--- Source/NuclearSimulationLayer.h
+++ Source/NuclearSimulationLayer.h
@@ -26,18 +26,23 @@ private:
     std::unique_ptr<Framebuffer> m_Framebuffer;
     std::unique_ptr<Shader> m_Shader;
 
+    const float m_ControlRodReactivity = 0.1f;
+    const float m_ControlRodSpeed = 10.0f;
     float m_ControlRodInsertion = 50.0f;
-    const float m_ControlRodSpeed = 5.0f;
+    bool m_AutomaticControl = true;
 
-    int64_t N = 1000000; // Neutron population
-    int64_t C = 1000000000000000; // Precursor concentration
-    float p0 = 0.01f;
-    float p = p0; // Reactivity
-    float Q; // Decay heat
-    float t = 0.0f; // Total time
+    const float dt = 0.01f; // Fixed Delta Time
 
-    float T0 = 250.0f;
-    float T = T0; // Temperature
-    float T_coolant = 200.0f; // Coolant temperature
-    float P = 1.0f; // Power
+    const float Tenv = 300.0f; // Ambient Temperature (C)
+    const float T0 = 300.0f; // Initial Core Temperature (C)
+    const float a = -0.000005f; // Temperature Coefficient (1/C)
+    const float y = 0.1f; // Cooling coefficient (1/s)
+    const float e = 0.01f; // Power to Heat coefficient (C/(N * s))
+    float T = T0; // Core Temperature (C)
+
+    const float A = 0.003f; // Neutron Generation Time (s)
+    float p = -0.01f; // Reactivity
+    float N = 1000.0f; // Neutron Density
+    float P = 0.0f; // Power Level
+    float Ptarget = 3200.0f;
 };
